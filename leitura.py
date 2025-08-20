0f04404c1465515590b16cefc4a65adcf64d6625
raisidoro <depto.ti04@gdbr-tg.com.br>
2025-08-20 16:08:28 -0300

    Update - AP Pecval v4
    Merge das alterações feitas pela Laura e Para a Carol. Geração da planilha em funcionamento, no entanto, deve ser verificado a questão do apontamento de dois kanbans simultaneos

--- leitura.py
+++ leitura.py
@@ -1,87 +1,100 @@
-# importing openpyxl module
 import openpyxl as xl
-from openpyxl import Workbook
-from platform import java_ver
 import wx
-import openpyxl
 import datetime
 import os
 import shutil
 import getpass as gt
-escolha=['IE-INJECAO','IE-PINTURA','IE-MONTAGEM']
-rdtype=None
-rtypes=[]
-final=['SIM','NAO']
+from openpyxl.styles import Alignment
+from openpyxl.utils import get_column_letter
+
+setores = ['IE-INJECAO', 'IE-PINTURA', 'IE-MONTAGEM']
+opcoes  = ['SIM', 'NAO']
 
 def leitura():
-    usuario = gt.getuser()
-    resposta = 'SIM'
-    filename1 = f"C:\\Users\\{usuario}\\Desktop\\REPORT_Master__FUNCIONAL.xlsx"
-    original = r'\\files-gdbr01\\GDBR\\ADMINISTRATION\\IT\\Desenvolvimento\\Apontamentos de producao - Programas\\Um-a-Um_Homologado\\modelo\\REPORT_Master__FUNCIONAL.xlsx'
-    target = f"C:\\Users\\{usuario}\\Desktop\\REPORT_Master__FUNCIONAL.xlsx"
+    usuario   = gt.getuser()
+    continuar = 'SIM'
+    gerada    = f"C:\\Users\\{usuario}\\Desktop\\REPORT_Master__FUNCIONAL.xlsx"
+    original  = r'\\files-gdbr01\\GDBR\\ADMINISTRATION\\IT\\Desenvolvimento\\Apontamentos de producao - Programas\\Um-a-Um_Homologado\\modelo\\REPORT_Master__FUNCIONAL.xlsx'
+    target    = gerada
 
     shutil.copyfile(original, target)
-    wb_modelo = xl.load_workbook(filename1, data_only=True)
+    wb_modelo    = xl.load_workbook(gerada, data_only=True)
     ws_modeloPec = wb_modelo['REPORT']
-    cont = 2
 
-    while resposta == 'SIM':
-        with wx.FileDialog(None, "Open XYZ file", wildcard="Excel Files (*.xlsm;*.xlsx)|*.xlsm;*.xlsx", style=wx.FD_OPEN | wx.FD_FILE_MUST_EXIST) as fileDialog:
+    cont = 2
+    while continuar == 'SIM':
+
+        with wx.FileDialog(None, "Selecione arquivos Excel", wildcard="Excel Files (*.xlsm;*.xlsx)|*.xlsm;*.xlsx", style=wx.FD_OPEN | wx.FD_FILE_MUST_EXIST | wx.FD_MULTIPLE) as fileDialog:
+            
             if fileDialog.ShowModal() == wx.ID_CANCEL:
                 return
-            pathname = fileDialog.GetPath()
-            print(f"\nOrigem: {pathname}")
+            
+            pathnames = fileDialog.GetPaths()
+            print(f"\nArquivos selecionados: {pathnames}")
 
-        dlg = wx.SingleChoiceDialog(None, ("Escolha seu setor"), "SETOR", escolha)
+        dlg = wx.SingleChoiceDialog(None, ("Escolha seu setor"), "SETOR", setores)
+
         if dlg.ShowModal() == wx.ID_OK:
             setor = dlg.GetStringSelection()
             print(f"\nSetor: {setor}")
-            escolha.remove(setor)
+            setores.remove(setor)
         dlg.Destroy()
 
-        wb_reporte = xl.load_workbook(pathname, data_only=True)
-        ws_reporte = wb_reporte['REPORT']
+        for pathname in pathnames:
 
-    #    wb_modelo = xl.load_workbook(filename1, data_only=True)
-     #   ws_modeloPec = wb_modelo['REPORT']
+            print(f"\nProcessando: {pathname}")
 
-        maxcol  = ws_reporte.max_column
-        maxrow  = ws_reporte.max_row
-        i =2
-        col_reporte = [ws_reporte.cell(row=1, column=j).value for j in range(1, maxcol+1)]
-        col_modelo  = [ws_modeloPec.cell(row=11, column=j).value for j in range(1, maxcol+1)]
+            wb_reporte = xl.load_workbook(pathname, data_only=True)
+            ws_reporte = wb_reporte['REPORT']
 
-        #cont = 2
+            maxcol = ws_reporte.max_column
+            maxrow = ws_reporte.max_row
+
+            col_reporte = [ws_reporte.cell(row=1, column=j).value for j in range(1, maxcol+1)]
+
+            colunas_ignoradas = []
+            for idx, col_name in enumerate(col_reporte, start=1):
+                if col_name in ["QTD REPINTURA", "QTD RETRABALHO", "QTD SCRAP", "QTD REVISÃO"]:
+                    colunas_ignoradas.append(idx)
+
+            # i --> linha
+            # j --> coluna
+            for i in range(2, maxrow+1):
+                val1 = ws_reporte.cell(row=i, column=1).value #Coluna1
+                val2 = ws_reporte.cell(row=i, column=2).value #Coluna2
+
+                # Verifica se as colunas 1 e 2 possuem valores
+                if val1 is not None and val2 not in (None, ""):
+                    # Processa as colunas restantes se tiver valor nas colunas 1 e 2
+                    for j in range(1, maxcol+1):
+                        # Ignora colunas que não devem ser processadas
+                        if j in colunas_ignoradas:
+                            continue
+                        # Obtém o valor da célula
+                        valor_celula = ws_reporte.cell(row=i, column=j).value
         
-        skip_columns = []
-        for idx, col_name in enumerate(col_reporte, start=1):
-            if col_name in ["QTD REPINTURA", "QTD RETRABALHO", "QTD SCRAP", "QTD REVISÃO"]:
-                skip_columns.append(idx)
+                        if valor_celula is None or valor_celula == "":
+                            if 7 <= j <= 33 or j == 5:
+                                valor_celula = ""
 
-        for i in range(i, maxrow+1):
-            # Só copia se tem dados relevantes
-            val1 = ws_reporte.cell(row=i, column=1).value
-            val2 = ws_reporte.cell(row=i, column=2).value
-            
-            if val1 is not None and val2 not in (None, ""):
-                for j in range(1, maxcol+1):
-                    if j in skip_columns:
-                        continue
-                    v = ws_reporte.cell(row=i, column=j).value
-                    if v is None or v == "":
-                        if 7 <= j <= 33 or j == 5:
-                            v = ""
-                         
-                    # # coluna "DATA DO DIA"
-                    # if ("DATA" or "DATA DO DIA") in col_reporte and isinstance(v, datetime.datetime):
-                    #     v = v.date()  
-                    #     ws_modeloPec.cell(row=cont, column=j).number_format = "DD/MM/YYYY"
+                        # Formata a célula como data, se necessário
+                        if ("DATA" in col_reporte or "DATA DO DIA" in col_reporte) and isinstance(valor_celula, datetime.datetime):
+                            valor_celula = valor_celula.date()
+                            ws_modeloPec.cell(row=cont, column=j).number_format = "DD/MM/YYYY"
 
-                    ws_modeloPec.cell(row=cont, column=j).value = v
-            cont = cont + 1
-        wb_modelo.save(filename1)
+                        ws_modeloPec.cell(row=cont, column=j).value = valor_celula
+                        ws_modeloPec.cell(row=cont, column=j).alignment = Alignment(horizontal='center')
 
-        dlg = wx.SingleChoiceDialog(None, ("Deseja continuar?"), "CONTINUAR", final)
+                    cont += 1
+
+        for col in ws_modeloPec.columns:
+            max_length = max(len(str(cell.value)) if cell.value else 0 for cell in col)
+            ws_modeloPec.column_dimensions[get_column_letter(col[0].column)].width = max_length + 2
+
+        wb_modelo.save(gerada)
+
+        dlg = wx.SingleChoiceDialog(None, ("Deseja continuar?"), "CONTINUAR", opcoes)
+
         if dlg.ShowModal() == wx.ID_OK:
-            resposta = dlg.GetStringSelection()
+            continuar = dlg.GetStringSelection()
         dlg.Destroy()

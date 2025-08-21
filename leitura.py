e5f890d938384d490f94d856430bf94f99f99580
lauraandrelaa <laura.andrela@gdbr-tg.com.br>
2025-08-21 12:05:38 -0300

    Update Apontamento Pecval
    Corrigir a planilha corrompida

--- leitura.py
+++ leitura.py
@@ -6,6 +6,7 @@ import shutil
 import getpass as gt
 from openpyxl.styles import Alignment
 from openpyxl.utils import get_column_letter
+from openpyxl.cell.cell import MergedCell
 
 setores = ['IE-INJECAO', 'IE-PINTURA', 'IE-MONTAGEM']
 opcoes  = ['SIM', 'NAO']
@@ -13,15 +14,15 @@ opcoes  = ['SIM', 'NAO']
 def leitura():
     usuario   = gt.getuser()
     continuar = 'SIM'
-    gerada    = f"C:\\Users\\{usuario}\\Desktop\\REPORT_Master__FUNCIONAL.xlsx"
-    original  = r'\\files-gdbr01\\GDBR\\ADMINISTRATION\\IT\\Desenvolvimento\\Apontamentos de producao - Programas\\Um-a-Um_Homologado\\modelo\\REPORT_Master__FUNCIONAL.xlsx'
+    gerada    = f"C:\\Users\\{usuario}\\Desktop\\REV00 Planilha de apontamento - Pintura.xlsx"
+    original  = r'\\files-gdbr01\\GDBR\\ADMINISTRATION\\IT\\Desenvolvimento\\Apontamentos de producao - Programas\\Um-a-Um_Homologado\\modelo\\REV00 Planilha de apontamento - Pintura.xlsx'
     target    = gerada
 
     shutil.copyfile(original, target)
     wb_modelo    = xl.load_workbook(gerada, data_only=True)
-    ws_modeloPec = wb_modelo['REPORT']
+    ws_modeloPec = wb_modelo['Apontamentos']
 
-    cont = 2
+    cont = 12
     while continuar == 'SIM':
 
         with wx.FileDialog(None, "Selecione arquivos Excel", wildcard="Excel Files (*.xlsm;*.xlsx)|*.xlsm;*.xlsx", style=wx.FD_OPEN | wx.FD_FILE_MUST_EXIST | wx.FD_MULTIPLE) as fileDialog:
@@ -45,21 +46,14 @@ def leitura():
             print(f"\nProcessando: {pathname}")
 
             wb_reporte = xl.load_workbook(pathname, data_only=True)
-            ws_reporte = wb_reporte['REPORT']
+            ws_reporte = wb_reporte['Apontamentos']
 
             maxcol = ws_reporte.max_column
             maxrow = ws_reporte.max_row
 
-            col_reporte = [ws_reporte.cell(row=1, column=j).value for j in range(1, maxcol+1)]
-
-            colunas_ignoradas = []
-            for idx, col_name in enumerate(col_reporte, start=1):
-                if col_name in ["QTD REPINTURA", "QTD RETRABALHO", "QTD SCRAP", "QTD REVISÃO"]:
-                    colunas_ignoradas.append(idx)
-
             # i --> linha
             # j --> coluna
-            for i in range(2, maxrow+1):
+            for i in range(12, maxrow+1):
                 val1 = ws_reporte.cell(row=i, column=1).value #Coluna1
                 val2 = ws_reporte.cell(row=i, column=2).value #Coluna2
 
@@ -67,23 +61,17 @@ def leitura():
                 if val1 is not None and val2 not in (None, ""):
                     # Processa as colunas restantes se tiver valor nas colunas 1 e 2
                     for j in range(1, maxcol+1):
-                        # Ignora colunas que não devem ser processadas
-                        if j in colunas_ignoradas:
-                            continue
+
                         # Obtém o valor da célula
                         valor_celula = ws_reporte.cell(row=i, column=j).value
         
-                        if valor_celula is None or valor_celula == "":
-                            if 7 <= j <= 33 or j == 5:
-                                valor_celula = ""
-
-                        # Formata a célula como data, se necessário
-                        if ("DATA" in col_reporte or "DATA DO DIA" in col_reporte) and isinstance(valor_celula, datetime.datetime):
-                            valor_celula = valor_celula.date()
-                            ws_modeloPec.cell(row=cont, column=j).number_format = "DD/MM/YYYY"
-
-                        ws_modeloPec.cell(row=cont, column=j).value = valor_celula
-                        ws_modeloPec.cell(row=cont, column=j).alignment = Alignment(horizontal='center')
+                        #if valor_celula is None or valor_celula == "":
+                            #if 7 <= j <= 33 or j == 5:
+                                #valor_celula = ""
+                        if not isinstance(ws_modeloPec.cell(row=cont, column=j), MergedCell):
+                            print(valor_celula)
+                            ws_modeloPec.cell(row=cont, column=j).value = valor_celula
+                            ws_modeloPec.cell(row=cont, column=j).alignment = Alignment(horizontal='center')
 
                     cont += 1
 
